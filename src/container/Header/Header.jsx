305678e5ca71fd22ab36dbb767004539f140cb18
Prince Gupta <princegupta1586@gmail.com>
2023-01-30 18:59:08 +0530

    Create Header Section

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React from "react";
+import images from "../../constants/images";
+import { SubHeading } from "../../components";
 
-import './Header.css';
+import "./Header.css";
 
-const Header = () => (
-  <div>
-    Header
-  </div>
-);
+const Header = () => {
+  return (
+    <div className="app__header app__wrapper section__padding">
+      <div className="app__wrapper_info">
+        <SubHeading title="Chase The New Flavour" />
+        <h1 className="app__header-h1">The Key to Fine Dining</h1>
+        <p className="p__opensans" style={{ margin: "2rem 0" }}>
+          Sit tellus lobortis sed senectus vivamus molestie. Condimentum
+          volutpat morbi facilisis quam scelerisque sapien. Et, penatibus
+          aliquam amet tellus
+        </p>
+        <button type="button" className="custom__button">
+          Explore Menu
+        </button>
+      </div>
+      <div className="app__wrapper_img">
+        <img src={images.welcome} alt="header img" />
+      </div>
+    </div>
+  );
+};
 
 export default Header;
