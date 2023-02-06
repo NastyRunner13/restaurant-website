c6e25033b965df3c1f95083cf308b31ac919c5c3
Prince Gupta <princegupta1586@gmail.com>
2023-02-06 17:34:26 +0530

    Create Chef Section

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -28,6 +28,11 @@ const Chef = () => {
             nunc.
           </p>
         </div>
+        <div className="app__chef-sign">
+          <p>Kevin Luo</p>
+          <p className="p__opensans">Chef & Founder</p>
+          <img src={images.sign} alt="sign" />
+        </div>
       </div>
     </div>
   );
